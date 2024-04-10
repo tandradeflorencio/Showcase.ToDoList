acdc93cb29e3405ad6d7bad39e10b64f5fcd4470
Thiago Andrade Florencio <t.andradeflorencio@gmail.com>
2024-04-10 18:18:22 -0300

    Adding required validation and adjusting docker compose.

--- src/Domain/Models/Requests/CreateToDoRequest.cs
+++ src/Domain/Models/Requests/CreateToDoRequest.cs
@@ -4,6 +4,7 @@ namespace Showcase.ToDoList.Domain.Models.Requests
 {
     public class CreateToDoRequest
     {
+        [Required(ErrorMessage = "Invalid Title")]
         [Length(1, 200, ErrorMessage = "Invalid Title")]
         public string? Title { get; set; }
     }
